9e599365224ce416cae07964495b81f40bfc98d0
kamrenidar <kamrenaidar@gmail.com>
2023-11-30 23:11:00 -0600

    final touches

--- Final project/Assets/ResolutionOptionsScript.cs
+++ Final project/Assets/ResolutionOptionsScript.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ResolutionOptionsScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] Dropdown resolutionDD;
+    [SerializeField] Toggle toggle;
+
+    Resolution[] resolutions;
+    
+
+    void Start(){
+        resolutions = Screen.resolutions;
+        toggle.isOn = PlayerPrefs.GetInt("fullscreen") == 0;
+
+        bool setDefault = false;
+        if(PlayerPrefs.GetInt("set default resolution") == 0){
+            setDefault = true;
+            PlayerPrefs.SetInt("set default resolution", 1);
+        }
+
+        for(int i = 0; i < resolutions.Length; i++){
+            string resolutionString = resolutions[i].width.ToString() + " x " + resolutions[i].height.ToString();
+            resolutionDD.options.Add(new Dropdown.OptionData(resolutionString));
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height){
+                resolutionDD.value = i;
+            }
+        }
+        resolutionDD.value = PlayerPrefs.GetInt("Resolution Selection");
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
+    
+    public void ChangeResolution(){
+        Screen.SetResolution(resolutions[resolutionDD.value].width, resolutions[resolutionDD.value].height, toggle.isOn);
+        PlayerPrefs.SetInt("Resolution Selection", resolutionDD.value);
+        if(toggle.isOn) PlayerPrefs.SetInt("fullscreen",0);
+        else PlayerPrefs.SetInt("fullscreen",1);
         
     }
 }
--- Final project/Assets/SoundOptionsScript.cs
+++ Final project/Assets/SoundOptionsScript.cs
@@ -8,17 +8,41 @@ public class SoundOptionsScript : MonoBehaviour
 {
     [SerializeField] AudioMixer am;
     [SerializeField] Slider masterSlider;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
+    void Start(){
+        if(PlayerPrefs.GetInt("Default Volume Changed") == 0){
+            masterSlider.value = 0.15f;
+            musicSlider.value = 0.15f;
+            sfxSlider.value = 0.15f;
+            PlayerPrefs.SetInt("Default Volume Changed",1);
+        }
+        masterSlider.value = PlayerPrefs.GetFloat("Master");
+        musicSlider.value = PlayerPrefs.GetFloat("Music");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfx");
+    }
 
     void SetVolume(string name, Slider slider){
-        //float volume = Mathf.Log10(slider.value) * 20;
-        //if(slider.value == 0){
-        //    volume = -80;
-        //}
-        am.SetFloat(name, slider.value);
-        //PlayerPrefs
+        PlayerPrefs.SetFloat(name, slider.value);
+
+        float volume = Mathf.Log10(slider.value) * 20;
+        if(slider.value == 0){
+            volume = -80;
+        }
+        am.SetFloat(name, volume);
+        PlayerPrefs.SetFloat(name, slider.value);
     }
 
     public void SetMasterVolume(){
         SetVolume("Master", masterSlider);
+    }
+
+    public void SetMusicVolume(){
+        SetVolume("Music", musicSlider);
+    }
+
+    public void SetSFXVolume(){
+        SetVolume("sfx", sfxSlider);
     }
 }
